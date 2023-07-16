16ed25f7c2c624f42fbed9115a2b1fec2ebdaf99
Nay907 <nayanika.mishra02@gmail.com>
2023-07-16 13:14:15 +0530

    first commit

---
+++ j12_whileloops.java
@@ -0,0 +1,20 @@
+
+
+public class j12_whileloops {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i<=3){
+            System.out.println(i);
+            i++;
+        }
+ //       while(true){
+ //           System.out.println("im hero");
+ //       }
+
+
+        
+
+        
+    }
+    
+}
