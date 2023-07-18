442794ecab6050df1f70404cbcf615a23f7703ad
Nay907 <nayanika.mishra02@gmail.com>
2023-07-18 13:14:15 +0530

    first commit

---
+++ j13_dowhileloop.java
@@ -0,0 +1,25 @@
+public class j13_dowhileloop {
+    public static void main(String[] args) {
+/*        int a =0;
+    do{
+        System.out.println(a);
+        a++;
+    }
+    while(a<5);*/
+ /*   int b =10;
+    do{
+        System.out.println(b);
+        b++;
+    }
+    while(b<5);*/
+    int c = 0;
+    do{
+        System.out.println(c);
+        c++;
+    }
+    while(c<=50);
+        
+    }
+    
+}    
+
