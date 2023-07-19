ca6b36ecd4bd17b110d65bc8ab5a097ad0bbd5a8
Nay907 <nayanika.mishra02@gmail.com>
2023-07-19 13:14:15 +0530

    first commit

---
+++ j14_forloop.java
@@ -0,0 +1,18 @@
+
+
+public class j14_forloop {
+    public static void main(String[] args){
+        for(int i = 0; i<=5; i++){
+            System.out.println(i);
+        }
+        
+        
+
+    }
+        
+}        
+    
+    
+
+ 
+
