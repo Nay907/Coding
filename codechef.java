2544a2bf23077045dac4f00388b658d6ec0f9b74
Nay907 <nayanika.mishra02@gmail.com>
2023-09-06 13:14:15 +0530

    first commit

---
+++ codechef.java
@@ -0,0 +1,10 @@
+public class codechef {
+    public static void main(String[] args) {
+        String a ="kartikey";
+        System.out.println(a.substring(0,4));
+        System.out.println(a);
+       String b = "";
+       System.out.println(b+a);
+    }
+    
+}
